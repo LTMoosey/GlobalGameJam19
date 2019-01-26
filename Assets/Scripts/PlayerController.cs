6fd8105a34df439b8602f91d1b699f1eece1cc37
Moosey <charlesbradley10@gmail.com>
2019-01-25 20:08:57 -0700

    created player and fire. movement works

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //movement variable
+    public float movementSpeed = 20f;
+
+    //torch object (i'll make this later)
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Move();
+	}
+
+    //call this to move the player 1 frame
+    private void Move()
+    {
+        Vector3 XMovement = Vector3.right * movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 YMovement = Vector3.up * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        transform.Translate(XMovement);
+        transform.Translate(YMovement);
+
+
+
+    }
+}
