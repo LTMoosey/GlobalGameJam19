c5ca5d6da9c5cce7130f5a7c1f2a77d66ed460cf
Moosey <charlesbradley10@gmail.com>
2019-01-26 15:16:24 -0700

    pulling to update to branch

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -7,13 +7,17 @@ public class AIController : MonoBehaviour
 {
     public Transform Player;
     public float moveSpeed;
-    private float rotateSpeed = 10;
+    private float rotateSpeed = 10f;
+    public float maxSpeed = 100f;
+    public float acceleration = 2f;
     private float distanceToPlayer;
+    private Rigidbody2D rigidbody;
+    public float viewDistance = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,13 +33,30 @@ public class AIController : MonoBehaviour
         targ.z = 0f;
 
         Vector3 objectPos = transform.position;
+        Vector3 distanceVector = targ - objectPos;
         targ.x = targ.x - objectPos.x;
         targ.y = targ.y - objectPos.y;
 
-        float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle -90));
 
-        transform.position += transform.up * Time.deltaTime * 5;
+
+        float angle = (Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg) - 90;
+        if(angle < 90 && Vector3.Magnitude(distanceVector) < viewDistance)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            //transform.position = Vector3.Lerp(transform.position, targ, .05f);
+            if (Vector3.Magnitude(rigidbody.velocity) < maxSpeed)
+            {
+                rigidbody.AddForce(transform.up * acceleration);
+            }
+        }
+        else{
+            if (Vector3.Magnitude(rigidbody.velocity) > 0)
+            {
+                rigidbody.velocity = rigidbody.velocity * 0.9f * Time.deltaTime;
+            }
+        }
+
         ////calculate direction between enemy and player
         //var targetDir = Player.transform.position - transform.position;
 
@@ -52,4 +73,23 @@ public class AIController : MonoBehaviour
         //if it is, the player is in the field of view
         return true;
     }
+
+    //void FollowTargetWithRotation(Transform target, float distanceToStop, float speed)
+    //{
+        //if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        //{
+        //    transform.LookAt(target);
+        //    //rigidbody.AddRelativeForce(Vector3.up * speed);
+        //}
+    //}
+
+    //void FollowTargetWitouthRotation(Transform target, float distanceToStop, float speed)
+    //{
+    //    var direction = Vector3.zero;
+    //    if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+    //    {
+    //        direction = target.position - transform.position;
+    //        rigidbody.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+    //    }
+    //}
 }
