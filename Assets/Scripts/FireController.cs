476c5f9976effa7eeb0aa4dca4be575ecb8ac47f
Moosey <charlesbradley10@gmail.com>
2019-01-25 20:52:23 -0700

    updated to latest version

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireController : MonoBehaviour {
+    public float Fuel = 100;
+    public float fuelPerSecond = 1;
+
+
 
 	// Use this for initialization
-	void Start () {
-		
+	IEnumerator Start () {
+		while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            OutputFuel();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        burnFuel();
 	}
+
+    void burnFuel()
+    {
+        Fuel -= fuelPerSecond * Time.deltaTime;
+    }
+
+    void giveFuel(float amount)
+    {
+        
+    }
+
+    void receiveFuel(float amount)
+    {
+        
+    }
+
+    //for testing purposes only
+    void OutputFuel()
+    {
+        Debug.Log("Remaining Fuel: " + Fuel);
+    }
 }
