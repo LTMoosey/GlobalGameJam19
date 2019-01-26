e3c08c9a76223e8b84af887b1d43b10c3d01beaa
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2019-01-26 12:45:46 -0700

    Modified 2d lighting to work with dynamic objects

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Light2D.Examples {
+    public class PlayerFollow : MonoBehaviour {
+        public GameObject player;
+
+        private void LateUpdate () {
+            Vector3 pos = player.transform.position;
+            pos.z = transform.position.z;
+            transform.position = pos;
+        }
+    }
+}
