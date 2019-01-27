b082073155c873bd0ed6ec4f3936fb5f2fc2fba9
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2019-01-26 18:14:49 -0700

    Added main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public GameObject player;
+    public Canvas mainMenu;
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start () {
+        player.GetComponent<PlayerController>().enabled = false;
+        mainMenu.enabled = true;
+        startButton.onClick.AddListener (startClicked);
+    }
+
+    void startClicked () {
+        player.GetComponent<PlayerController>().enabled = true;
+        mainMenu.enabled = false;
+    }
+}
